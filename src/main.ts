86a9b92a29beb727b8b453b4d52520cf42785ede
Alexander Gerhard <870717ag@gmail.com>
2026-02-06 07:05:16 +0100

    chore: configure GitHub Pages deploy with Vite build

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
+// @ts-nocheck
+
 import './style.css';
 
-document.addEventListener('click', (e) => {
+
+/* BUTTON SUBMIT */
+const registerBtn = document.querySelector('#register');
+registerBtn.addEventListener('click', registerExpense);
+
+function registerExpense(e) {
+  e.preventDefault();
+
+  console.log('registrera belopp och text i en array');
+  
+}
+
+
+
+
+/* DELETE BUTTON */
+document.addEventListener('click', deleteRow);
+
+function deleteRow(e: Event) {
   const target = e.target as HTMLElement;
 
-  if (target.classList.contains('delete-row')) {
-    target.closest('.row')?.remove();
-  }
-});
+  if (!target.classList.contains('delete-row')) return;
+
+  target.closest('.row').remove();
+}
