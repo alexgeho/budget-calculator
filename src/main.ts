6285b71324bc42070cd5115838c05334bed3307e
Alexander Gerhard <870717ag@gmail.com>
2026-02-06 14:25:55 +0100

    fix: correct Enter key handling and localStorage key usage

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,71 @@
 
 import './style.css';
 
+let myData = [];
+ 
+
+const input = document.querySelector('#addItem')
+const writeBtn = document.querySelector('#write')
+const readBtn = document.querySelector('#read')
+
+readBtn?.addEventListener('click', readFromLocalStorage)
+
+input?.addEventListener('keydown', checkInputConfirm);
+
+function checkInputConfirm(e) {
+
+const ENTER_KEYCODE = 13;
+
+  if (!e.keyCode === ENTER_KEYCODE) {
+    return;
+  } else {
+    console.log('Yeeeei');
+    
+  }
+
+myData.push(input.value)
+
+console.log(myData);
+
+saveToLocalStorage();
+}
+
+function saveToLocalStorage(params:type) {
+  const stringified = JSON.stringify(myData)
+
+localStorage.setItem('mySavedStuff', stringified)  
+console.log('Data saved');
+
+}
+
+
+function readFromLocalStorage() {
+const savedValue = localStorage.getItem('mySavedStuff');
+
+myData = JSON.parse(savedValue);
+
+console.log('myData är nu', myData);
+
+}
+
+
+
+
+
+/* HIDE TAGS */
+
+function toggleTagsHide(hide: boolean) {
+  document.querySelectorAll('form, section, header, footer').forEach(el => {
+    (el as HTMLElement).hidden = hide;
+  });
+}
+
+toggleTagsHide(true);
+
+/* HIDE TAGS */
+
+
+
 
 /* BUTTON SUBMIT */
 const registerBtn = document.querySelector('#register');
@@ -11,9 +76,8 @@ function registerExpense(e) {
   e.preventDefault();
 
   console.log('registrera belopp och text i en array');
-  
-}
 
+}
 
 
 
---
+++ tankar.md
@@ -0,0 +1,26 @@
+Du ska skapa en liten "budget-app". I appen ska det finnas med:
+
+Ett fält för att mata in en utgift (belopp och beskrivning)
+
+label
+input för texten
+
+label
+input för beloppet
+
+spara knapp med event
+
+> lagra inforamtionen någonstans
+> skriva ut inforamtionen på sidan
+
+vad händer sen när jag har en till utgift?
+
+
+Ett fält för att mata in en inkomst (belopp och beskrivning)
+Bredvid varje budgetpost ska det finnas en radera-knapp
+Det ska visas en balans (inkomster minus utgifter)
+Balansens ska färgkodas beroende på om det är ett positivt eller negativt värde
+Balansen ska uppdateras varje gång en ny utgift eller inkomst matas in
+Till varje budgetpost ska det gå att välja en kategori från en dropdown-lista (select)
+Informationen ska sparas i local storage så att när användaren kommer till sidan nästa gång, så ska informationen finnas kvar. Nytt
+Kategorierna ska läsas in via JSON. Vi går igenom detta på lektionen. Nytt
