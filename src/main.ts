02efbf198c8b5b5202682949a30f79e240ed4dc9
Alexander Gerhard <870717ag@gmail.com>
2026-02-13 17:26:52 +0100

    feat: implement basic transaction flow and store data in array

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,62 @@
 import './style.css'
 
 
+let budgetPosters: Itransaction[] = [];
+
+interface Itransaction {
+  descrition: string;
+  amount: number;
+  type: 'expence' | 'income';
+}
+
+const form = document.querySelector('form')
+
+form?.addEventListener('submit', saveToStorage)
+
+function saveToStorage(e: SubmitEvent) {
+  e.preventDefault()
+
+  const description = document.querySelector<HTMLInputElement>('#desc')
+  const amount = document.querySelector<HTMLInputElement>('#amount')
+  const type = document.querySelector<HTMLInputElement>('input[name="type"]:checked')
+
+  if (!description || !amount || !type) {
+    return
+  }
+
+  const descriptionValue = description.value.trim()
+  const amountValue = Number(amount.value)
+  const typeValue = type.value as 'expence' | 'income';
+
+  if (!descriptionValue || amountValue <= 0) {
+    return
+  }
+
+  const budgetPoster: Itransaction = {
+    descrition: descriptionValue,
+    amount: amountValue,
+    type: typeValue
+  }
+
+
+budgetPosters.push(budgetPoster)
+
+if (budgetPosters) {
+  console.log('budgetPosters::::', budgetPosters);
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+// if (descrition) {
+//   descrition.innerHTML = 'Hello brother'
+// }
+
--- tankar.md
+++ tankar.md
@@ -1,21 +1,6 @@
 Du ska skapa en liten "budget-app". I appen ska det finnas med:
 
 + Ett fält för att mata in en utgift (belopp och beskrivning)
-
-label
-input för texten
-
-label
-input för beloppet
-
-spara knapp med event
-
-> lagra inforamtionen någonstans
-> skriva ut inforamtionen på sidan
-
-vad händer sen när jag har en till utgift?
-
-
 + Ett fält för att mata in en inkomst (belopp och beskrivning)
 + Bredvid varje budgetpost ska det finnas en radera-knapp
 + Det ska visas en balans (inkomster minus utgifter)
@@ -23,5 +8,10 @@ vad händer sen när jag har en till utgift?
 + Balansen ska uppdateras varje gång en ny utgift eller inkomst matas in
 + Till varje budgetpost ska det gå att välja en kategori från en dropdown-lista (select)
 + Informationen ska sparas i local storage så att när användaren kommer till sidan nästa gång, så ska informationen finnas kvar. Nytt
-
 + Kategorierna ska läsas in via JSON. Vi går igenom detta på lektionen. Nytt
+
+Planeen är så:
+
+1. Skapar Arrey 
+2. Allt input sparas 
+3. Sätter det arrety to local storage
